6482822dffa8dd055218dd4bbfe954f74d62901c
Leif Segen <leif.nabil.segen@gmail.com>
2021-09-08 21:27:55 -0500

    🧪 logger & balance util tests

--- src/utils/logger.utils.test.ts
+++ src/utils/logger.utils.test.ts
@@ -1,11 +1,10 @@
 import { mockRequest, mockResponse } from '../tests/tests.utils';
-import { loggingMiddleware, messageWrapper } from "./logger.utils";
+import { loggingMiddleware, messageWrapper } from './logger.utils';
 
 describe('loggingMiddleware', () => {
   xit('should debug the requested route and request method', () => {
     const next = jest.fn().mockReturnValue(undefined);
     loggingMiddleware(mockRequest(), mockResponse(), next);
-    
   });
   it('should call the next function', () => {
     const next = jest.fn().mockReturnValue(undefined);
@@ -16,6 +15,8 @@ describe('loggingMiddleware', () => {
 
 describe('messageWrapper', () => {
   it('should wrap a message with a prefix', () => {
-    expect(messageWrapper('aPrefix', 'aMessage')).toStrictEqual('aPrefix|aMessage');
+    expect(messageWrapper('aPrefix', 'aMessage')).toStrictEqual(
+      'aPrefix|aMessage'
+    );
   });
 });
--- src/utils/trading/balance.utils.test.ts
+++ src/utils/trading/balance.utils.test.ts
@@ -1,28 +1,41 @@
 import { IBalance } from '../../interfaces/exchanges/common.exchange.interfaces';
 import { ExchangeId } from '../../constants/exchanges.constants';
 
-import { sampleBinanceSpotBalance, sampleBinanceFuturesUSDBalance } from '../../tests/fixtures/binance.fixtures';
+import {
+  sampleBinanceSpotBalance,
+  sampleBinanceFuturesUSDBalance
+} from '../../tests/fixtures/binance.fixtures';
 import { filterBalances } from './balance.utils';
 
 describe('filterBalances', () => {
   it('should find symbol, free amount, and total amount for Binance', () => {
-    const sampleBinanceSpotBalances = { info: { balances: [
-      sampleBinanceSpotBalance
-    ] } };
-    const result = filterBalances(sampleBinanceSpotBalances, ExchangeId.Binance) as IBalance[];
+    const sampleBinanceSpotBalances = {
+      info: { balances: [sampleBinanceSpotBalance] }
+    };
+    const result = filterBalances(
+      sampleBinanceSpotBalances,
+      ExchangeId.Binance
+    ) as IBalance[];
     expect(result[0].coin).toBe(sampleBinanceSpotBalance.asset);
     expect(result[0].free).toBe(sampleBinanceSpotBalance.free);
-    expect(result[0].total).toBe(parseFloat(sampleBinanceSpotBalance.free) 
-      + parseFloat(sampleBinanceSpotBalance.locked));
+    expect(result[0].total).toBe(
+      parseFloat(sampleBinanceSpotBalance.free) +
+        parseFloat(sampleBinanceSpotBalance.locked)
+    );
   });
 
   it('should find symbol, free amount, and total amount for Binance Futures USD', () => {
-    const sampleBinanceFuturesUSDBalances = { info: { assets: [
-      sampleBinanceFuturesUSDBalance
-    ] } };
-    const result = filterBalances(sampleBinanceFuturesUSDBalances, ExchangeId.BinanceFuturesUSD) as IBalance[];
+    const sampleBinanceFuturesUSDBalances = {
+      info: { assets: [sampleBinanceFuturesUSDBalance] }
+    };
+    const result = filterBalances(
+      sampleBinanceFuturesUSDBalances,
+      ExchangeId.BinanceFuturesUSD
+    ) as IBalance[];
     expect(result[0].coin).toBe(sampleBinanceFuturesUSDBalance.asset);
-    expect(result[0].free).toBe(sampleBinanceFuturesUSDBalance.availableBalance);
+    expect(result[0].free).toBe(
+      sampleBinanceFuturesUSDBalance.availableBalance
+    );
     expect(result[0].total).toBe(sampleBinanceFuturesUSDBalance.walletBalance);
   });
 });
