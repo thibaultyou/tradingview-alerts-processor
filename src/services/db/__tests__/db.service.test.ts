9bf688d5ffb544ec6cba3017ebbe27764c5961e0
Leif Segen <leif.nabil.segen@gmail.com>
2021-09-19 01:51:51 -0500

    🧪 Add a few tests.
    Some refactor to allow for setting which database type is to be expected

--- src/services/db/__tests__/db.service.test.ts
+++ src/services/db/__tests__/db.service.test.ts
@@ -1,11 +1,49 @@
-describe('Database service', () => {
-  describe('getDatabaseInstance', () => {
-    it.todo('should return RedisDatabaseService instance');
+import { DATABASE_CONFIGURATION_ERROR } from '../../../messages/db.messages';
+import { DatabaseId } from '../../../constants/db.constants';
+import { DatabaseService } from '../db.service';
+import { JSONDatabaseService } from '../json.db.service';
+import { RedisDatabaseService } from '../redis.db.service';
 
-    it.todo('should return JSONDatabaseService instance');
+describe('Database service', () => {
+  describe('setDatabaseType', () => {
+    it('should set the database type', () => {
+      expect(() => {
+        DatabaseService.setDatabaseType(DatabaseId.JSON);
+      }).not.toThrow();
+    });
+
+    it('should throw an error for a missing or unrecognized database type', () => {
+      expect(() => {
+        DatabaseService.setDatabaseType('sider');
+      }).toThrow(DATABASE_CONFIGURATION_ERROR);
+    });
+  });
+
+  describe('getDatabaseInstance', () => {
+    it('should return RedisDatabaseService instance when Redis is set', () => {
+      DatabaseService.setDatabaseType(DatabaseId.REDIS);
+      expect(DatabaseService.getDatabaseInstance()).toBeInstanceOf(
+        RedisDatabaseService
+      );
+    });
+
+    it('should return JSONDatabaseService instance when JSON is set', () => {
+      DatabaseService.setDatabaseType(DatabaseId.JSON);
+      expect(DatabaseService.getDatabaseInstance()).toBeInstanceOf(
+        JSONDatabaseService
+      );
+    });
   });
 
   describe('getType', () => {
-    it.todo('should return database type');
+    it('should return database type', () => {
+      const supportedDatabaseTypes = [
+        DatabaseId.REDIS.toString(),
+        DatabaseId.JSON.toString()
+      ];
+      expect(
+        supportedDatabaseTypes.includes(DatabaseService.getType())
+      ).toBeTruthy();
+    });
   });
 });
--- src/services/db/db.service.ts
+++ src/services/db/db.service.ts
@@ -2,6 +2,8 @@ import { DatabaseId } from '../../constants/db.constants';
 import { JSONDatabaseService } from './json.db.service';
 import { RedisDatabaseService } from './redis.db.service';
 import { Database } from '../../types/db.types';
+import { error } from 'winston';
+import { DATABASE_CONFIGURATION_ERROR } from '../../messages/db.messages';
 
 const DATABASE = process.env.DATABASE_TYPE || DatabaseId.JSON;
 
@@ -11,12 +13,25 @@ export class DatabaseService {
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   private constructor() {}
 
+  public static setDatabaseType(_databaseType: string = null): void {
+    if (_databaseType) {
+      switch (_databaseType) {
+        case DatabaseId.JSON:
+          DatabaseService.instance = new JSONDatabaseService();
+          break;
+        case DatabaseId.REDIS:
+          DatabaseService.instance = new RedisDatabaseService();
+          break;
+        default:
+          error(DATABASE_CONFIGURATION_ERROR);
+          throw new Error(DATABASE_CONFIGURATION_ERROR);
+      }
+    }
+  }
+
   public static getDatabaseInstance = (): Database => {
     if (!DatabaseService.instance) {
-      DatabaseService.instance =
-        DATABASE === DatabaseId.REDIS
-          ? new RedisDatabaseService()
-          : new JSONDatabaseService();
+      this.setDatabaseType(DATABASE);
     }
     return DatabaseService.instance;
   };
