c3dbb0a5c035b33200ccd093636193317e546a0d
Leif Segen <leif.nabil.segen@gmail.com>
2021-09-19 18:15:02 -0500

    🧪 Add two more db service test

--- src/services/db/__tests__/json.service.test.ts
+++ src/services/db/__tests__/json.service.test.ts
@@ -1,18 +1,56 @@
+import { JsonDB } from 'node-json-db';
 import { JSONDatabaseService } from '../json.db.service';
 
 describe('JSON database service', () => {
   describe('constructor', () => {
+    let mockClientCreator;
+
     it('should apply config', () => {
-      const mockClient = jest.fn();
-      new JSONDatabaseService(mockClient);
-      expect(mockClient).toHaveBeenCalled();
+      mockClientCreator = jest.fn();
+      new JSONDatabaseService(mockClientCreator);
+      expect(mockClientCreator).toHaveBeenCalled();
     });
   });
 
   describe('read', () => {
-    it.todo('should return entry');
+    /**
+     * Confirms that the provided key is prepended with a '/',
+     * and that this.instance.getData() is called with such,
+     * and that its result is returned.
+     */
+    it('should return entry', () => {
+      const mockGetData = jest.fn(() => 'someData');
 
-    it.todo('should return error');
+      const mockClientCreator = () => {
+        const mockInstance = {} as JsonDB;
+        mockInstance.getData = mockGetData;
+        return mockInstance;
+      };
+
+      const key = 'aKey';
+      const service = new JSONDatabaseService(mockClientCreator);
+      const result = service.read(key);
+      expect(mockGetData).toHaveBeenCalledWith(`/${key}`);
+      expect(result).toEqual('someData');
+    });
+
+    it('should return error', () => {
+      const mockGetData = jest.fn(() => {
+        throw new Error();
+      });
+
+      const mockClientCreator = () => {
+        const mockInstance = {} as JsonDB;
+        mockInstance.getData = mockGetData;
+        return mockInstance;
+      };
+
+      const key = 'aKey';
+      const service = new JSONDatabaseService(mockClientCreator);
+      expect(() => {
+        service.read(key);
+      }).toThrow();
+    });
   });
 
   describe('write', () => {
