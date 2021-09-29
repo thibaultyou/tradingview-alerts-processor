d025228075bd63de5f5a53def211363864477fd0
Leif Segen <leif.nabil.segen@gmail.com>
2021-09-29 07:02:31 -0500

    :hammer_and_wrench: Add common utils tests

--- src/utils/exchanges/__tests__/ftx.utils.test.ts
+++ src/utils/exchanges/__tests__/ftx.utils.test.ts
@@ -4,21 +4,20 @@ import { isFTXSpot } from '../ftx.utils';
 describe('FTX utils', () => {
   describe('isFTXSpot', () => {
     it('should return true', () => {
-      const mockTicker = {
-        info: {
-          type: 'spot'
-        }
-      } as Ticker;
+      const mockTicker = { info: { type: 'spot' } } as Ticker;
       expect(isFTXSpot(mockTicker)).toBeTruthy();
     });
 
     it('should return false', () => {
-      const mockTicker = {
-        info: {
-          type: 'anythingElse'
-        }
-      } as Ticker;
+      const mockTicker = { info: { type: 'anythingElse' } } as Ticker;
       expect(isFTXSpot(mockTicker)).toBeFalsy();
+    });
+
+    it('should throw error', () => {
+      expect(() => isFTXSpot(undefined)).toThrowError(TypeError);
+      expect(() => isFTXSpot(null)).toThrowError(TypeError);
+      const mockTicker = {} as Ticker;
+      expect(() => isFTXSpot(mockTicker)).toThrowError();
     });
   });
 });
